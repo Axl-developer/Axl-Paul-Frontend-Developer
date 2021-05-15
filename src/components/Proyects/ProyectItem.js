e2f36fc29c14ee15af96edc8495768b89ad4050e
Axl Evangelista <peruaxl12@gmail.com>
2021-05-15 16:25:06 -0500

    Tweet Proyect ScreenShot

--- src/components/Proyects/ProyectItem.js
+++ src/components/Proyects/ProyectItem.js
@@ -17,7 +17,7 @@ export const ProyectItem = (props) => {
         <ProyectI className="content_proyect" animate={{opacity:1,transform:'translateY(0px)'}} initial={{opacity:0,transform:'translateY(20px)'}} exit={{opacity:0}}>
             <div className="sub_content_proyect">
                 <div className="content_img">
-                    <img src={`http://localhost:3000/${url_img}`} alt="twitter" />
+                    <img src={`https://portafolio-axl.herokuapp.com/${url_img}`} alt="twitter" />
                 </div>
                 <div className="content_text">
                     <h1>{name}</h1>
